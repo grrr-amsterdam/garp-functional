02f5b54ae830d9198bc935a47aac7d5ed205deb8
Harmen Janssen <harmen@grrr.nl>
2017-04-01 14:23:22 +0200

    Allow second parameter of prop_set to be callable

--- tests/PropSetTest.php
+++ tests/PropSetTest.php
@@ -46,4 +46,44 @@ class PropSetTest extends TestCase {
         );
     }
 
+    public function test_should_accept_a_function_as_2nd_parameter_which_gets_applied_to_the_3rd() {
+        $obj = array(
+            'first_name' => 'John',
+            'last_name' => 'Coltrane'
+        );
+        $setEmail = f\prop_set(
+            'email',
+            function ($o) {
+                return strtolower(f\concat(f\prop('first_name', $o), '@gmail.com'));
+            }
+        );
+        $this->assertEquals(
+            array(
+                'first_name' => 'John',
+                'last_name' => 'Coltrane',
+                'email' => 'john@gmail.com'
+            ),
+            $setEmail($obj)
+        );
+
+        $people = array(
+            array('name' => 'Alice'),
+            array('name' => 'Bob'),
+        );
+        $people = f\map(
+            f\prop_set(
+                'name',
+                f\compose('strrev', f\prop('name'))
+            ),
+            $people
+        );
+        $this->assertEquals(
+            array(
+                array('name' => 'ecilA'),
+                array('name' => 'boB')
+            ),
+            $people
+        );
+
+    }
 }
