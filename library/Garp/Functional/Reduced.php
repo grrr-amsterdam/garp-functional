6d6aa2ece470ae40a7e85447565323c5ae1e8743
Harmen Janssen <harmen@grrr.nl>
2019-01-25 13:10:37 +0100

    Refactor reduce to allow early return

---
+++ library/Garp/Functional/Reduced.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @package  Garp\Functional
+ * @author   Harmen Janssen <harmen@grrr.nl>
+ * @license  https://github.com/grrr-amsterdam/garp-functional/blob/master/LICENSE.md BSD-3-Clause
+ */
+namespace Garp\Functional;
+
+/**
+ * Declares a value as being "reduced".
+ * This signals a short-circuit to the `reduce` function.
+ *
+ * @param  mixed $value
+ * @return Garp\Functional\ReducedValue
+ */
+function reduced($value): ReducedValue {
+    return is_reduced($value)
+        ? $value
+        : new ReducedValue($value);
+}
