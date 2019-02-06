bc3a4f14bb33728923b01caf3bec8f7b77e25c1b
Harmen Janssen <harmen@grrr.nl>
2019-02-06 22:43:57 +0100

    Remove superfluous docblock

--- _internal/ReducedValue.php
+++ _internal/ReducedValue.php
@@ -1,11 +1,6 @@
 <?php
 declare(strict_types=1);
 
-/**
- * @package  Garp\Functional
- * @author   Harmen Janssen <harmen@grrr.nl>
- * @license  https://github.com/grrr-amsterdam/garp-functional/blob/master/LICENSE.md BSD-3-Clause
- */
 namespace Garp\Functional\Internal;
 
 /**
