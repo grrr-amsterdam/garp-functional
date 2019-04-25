0eecefd4339b7dac296f586b0a07473ab945946e
Harmen Janssen <harmen@grrr.nl>
2019-04-25 09:33:10 +0200

    Add subtract to fix the typo in substract

--- tests/SubtractTest.php
+++ tests/SubtractTest.php
@@ -9,17 +9,17 @@ use Garp\Functional as f;
  */
 class SubtractTest extends TestCase {
 
-    public function test_should_substract_numbers() {
-        $this->assertEquals(0, f\substract(5, 5));
-        $this->assertEquals(-5, f\substract(5, 0));
-        $this->assertEquals(10, f\substract(40, 50));
+    public function test_should_subtract_numbers() {
+        $this->assertEquals(0, f\subtract(5, 5));
+        $this->assertEquals(-5, f\subtract(5, 0));
+        $this->assertEquals(10, f\subtract(40, 50));
     }
 
     public function test_should_be_curried() {
-        $substract5 = f\substract(5);
-        $this->assertTrue(is_callable($substract5));
-        $this->assertEquals(0, $substract5(5));
-        $this->assertEquals(5, $substract5(10));
+        $subtract5 = f\subtract(5);
+        $this->assertTrue(is_callable($subtract5));
+        $this->assertEquals(0, $subtract5(5));
+        $this->assertEquals(5, $subtract5(10));
     }
 
 }
