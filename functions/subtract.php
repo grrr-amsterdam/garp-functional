26fb5135417a67d04a469686d95c0c700c108bf7
Harmen Janssen <harmen@grrr.nl>
2019-04-25 09:33:10 +0200

    Add subtract to fix the typo in substract

--- functions/subtract.php
+++ functions/subtract.php
@@ -15,8 +15,26 @@ namespace Garp\Functional;
  * @param  int|float $left
  * @param  int|float $right
  * @return int|float
+ * @deprecated This function name contains a typo, and will be removed in the next major version. Use subtract instead.
+ * @see subtract
  */
 function substract($left, $right = null) {
+    trigger_error(
+        'substract is deprecated and will be removed in the next major version. Use Garp\Functional\subtract instead',
+        E_USER_DEPRECATED
+    );
+    return subtract(...func_get_args());
+}
+
+/**
+ * Subtract two numbers.
+ * Note that the first argument is subtracted from the last argument.
+ *
+ * @param  int|float $left
+ * @param  int|float $right
+ * @return int|float
+ */
+function subtract($left, $right = null) {
     return autocurry(
         function ($left, $right) {
             return $right - $left;
